c6a5bc95e501b84aa391454ad4741fc725c42304
Maksym Bernatskyi <bernasik007@gmail.com>
2022-01-20 23:53:41 +0200

    update HW-07 taks-01

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,25 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
-
 console.log(galleryItems);
+
+const mainGalleryList = document.querySelector('.gallery');
+const itemsMarkup = createGalleryItemsMarkup(galleryItems);
+
+mainGalleryList.insertAdjacentHTML('beforeend', itemsMarkup);
+
+function createGalleryItemsMarkup(items) {
+    return items.map(({ preview, original, description }) => {
+        return `
+        <div class="gallery__item">
+            <a class="gallery__link" href="${original}">
+                <img
+                class="gallery__image"
+                src="${preview}"
+                data-source="${original}"
+                alt="${description}"
+                />
+            </a>
+        </div>
+        `;
+    }).join('');
+}
